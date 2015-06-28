cf389788448166272a08ff0948cb9ef97190d3f6
Piotr Klimiec <klimiec@gmail.com>
2015-06-28 23:01:30 +0200

    Detecting edges algorithm ver 001

--- main.py
+++ main.py
@@ -9,12 +9,44 @@ cam = Camera(1, {'width': 640, 'height': 480})
 
 roi = {'x': sys.argv[1], 'y': sys.argv[2], 'w': sys.argv[3], 'h': sys.argv[4]}
 
-while True:
+# globals
+n = 20
+buffer = []
+avg = 0
+threshold = 3;
+
+def count_white_pixels():
     img = cam.getImage()
     img = img.crop(roi['x'], roi['y'], roi['w'], roi['h'])
     img = img.edges(50, 200)
     imgmat = img.getNumpy().flatten()
-    whitepx = cv2.countNonZero(imgmat) / 3072.0
-    print whitepx
-    img.show()
+    whitepx = cv2.countNonZero(imgmat) / (roi['w'] *  roi['h'])
+    return whitepx
+
+# prepare buffer
+for i in range(n):
+    buffer.append(count_white_pixels())
+
+avg = sum(buffer)/n
+
+while True:
+    whitepx = count_white_pixels()
+    current_diff = avg - whitepx
+
+    # check parking spot
+    if abd(current_diff) > threshold:
+        if current_diff > 0 :
+            print 'Przyjechalo +++'
+        else:
+            print "Odjechalo ---"
+
+    # update buffor
+    remove_element = buffer.pop(0)
+    buffer.append(whitepx)
+    avg += whitepx/n - remove_element/n
+
+
+
+
+
 
