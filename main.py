edce37015cdd6b8b421fee5cca7b91495e901c3a
Piotr Klimiec <klimiec@gmail.com>
2015-06-29 05:35:07 +0200

    Sending Tweets

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+# coding=utf-8
+
 from SimpleCV import *
 import sys
 import time
+from tweet import Tweet
 
 if len(sys.argv) < 4:
     print "Usage: main.py x y width height"
@@ -10,19 +13,30 @@ cam = Camera(1, {'width': 640, 'height': 480})
 
 roi = {'x': sys.argv[1], 'y': sys.argv[2], 'w': sys.argv[3], 'h': sys.argv[4]}
 
+twitter_configuration = {
+    'consumer_key' : '9yHE3TdYv9cVbVi5tme6C76Gj',
+    'consumer_secret' :  'noaO55Ra6jxWHYX4HDAo24PRskms7L8ZWMerNEAEeNCdOlbmvI',
+    'access_token' : '383755446-jhC6SSZ0NoeEz9FSiiQ4SKB1SAZ3KtFUJ3Vzxpbl',
+    'access_token_secret' : 'yfYvmFgfTCCWiJKug9zM2Yde169XUx75Jyt9sxywTGxQj'
+}
+
+tweet = Tweet(twitter_configuration)
+
 # globals
-n = 20
+n = 60
 buffer = []
 avg = 0
 threshold = 3
 current_diff = 0
+img = None
 
 def count_white_pixels():
+    global img
     img = cam.getImage()
     img = img.crop(roi['x'], roi['y'], roi['w'], roi['h'])
-    img = img.edges(50, 200)
-    imgmat = img.getNumpy().flatten()
-    whitepx = (cv2.countNonZero(imgmat) * 100) / (int(roi['w']) * int(roi['h']))
+    img_edge = img.edges(50, 200)
+    img_mat = img_edge.getNumpy().flatten()
+    whitepx = (cv2.countNonZero(img_mat) * 100) / (int(roi['w']) * int(roi['h']))
     return whitepx
 
 # prepare buffer
@@ -38,7 +52,6 @@ print 'avg = ' + str(avg)
 while True:
     whitepx = count_white_pixels()
     current_diff = avg - whitepx
-
     print 'Current: ' + str(whitepx) + "| avg: " + str(avg) + " | Diff: " +  str(current_diff)
 
     # check parking spot
@@ -49,18 +62,13 @@ while True:
         current_diff = avg - whitepx
         if abs(current_diff) > threshold:
             if current_diff > 0 :
-                print 'Przyjechalo +++'
+                print 'Vehicle arrived +++'
+                tweet.send_tweet(img, 'Przyjechała niezła bryka!')
             else:
-                print "Odjechalo ---"
+                print "Vehicle left ---"
+                tweet.send_tweet(img, 'Odjechała w siną dal!')
 
     # update buffer
     remove_element = buffer.pop(0)
     buffer.append(whitepx)
     avg = sum(buffer)/n
-
-
-
-
-
-
-
